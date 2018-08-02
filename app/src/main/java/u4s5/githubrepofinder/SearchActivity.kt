742de6a11d57e8db535fdb69e1f22c6dad68522f
Yury Salnikov <u4s5@yandex.ru>
2018-08-02 19:58:06 +0300

    Initial commit.

---
+++ app/src/main/java/u4s5/githubrepofinder/SearchActivity.kt
@@ -0,0 +1,12 @@
+package u4s5.githubrepofinder
+
+import android.app.Activity
+import android.os.Bundle
+
+class SearchActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
